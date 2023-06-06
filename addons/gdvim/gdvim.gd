625bb0e03781fffe45729906e8af0f9cf0b5c7c3
RAYMUNDOOOO <rtovar.develops@gmail.com>
2023-06-06 23:30:51 +1000

    Learned to update label in status container
    Can update status label in container upon receiving mouse input. Next step is to figure out how we know if we clicked on the Script Panel and then we can set the file path in the status line container.

--- addons/gdvim/gdvim.gd
+++ addons/gdvim/gdvim.gd
@@ -3,9 +3,13 @@ extends EditorPlugin
 
 # The container to display the status of the user
 var status_container : Node
+var editor_interface : EditorInterface
 
 func _enter_tree() -> void:
 	# INSTANTIATION
+	# Instantiating interfaces
+	editor_interface = get_editor_interface()
+	
 	# Instantiating the status container and loading it into the editor
 	status_container = preload("res://addons/gdvim/editor_control/status_container.tscn").instantiate()
 	add_control_to_container(EditorPlugin.CONTAINER_INSPECTOR_BOTTOM, status_container)
@@ -16,3 +20,14 @@ func _exit_tree() -> void:
 	# Removing the control node from the editor and freeing memory
 	remove_control_from_container(EditorPlugin.CONTAINER_INSPECTOR_BOTTOM, status_container)
 	status_container.free()
+
+# Handles input and then sends it off to relevant class
+func _input(event: InputEvent) -> void:
+	# If InputEvent was changing the script, send input to StatusContainerController
+	# If InputEvent was keyboard input, send to ModeController
+	# 	ModeController will then determine to send to NormalModeController
+	var mouse_input: InputEventMouseButton = event as InputEventMouseButton
+	if mouse_input:
+		if mouse_input.pressed:
+				var file_path : Label = status_container.get_node("FileInformationContainer/FilePath")
+				file_path.set_text(editor_interface.get_current_path()) # Doesn't work because it only gets path to script open in FileSystem dock
